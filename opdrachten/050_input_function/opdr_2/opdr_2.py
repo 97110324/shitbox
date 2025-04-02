3983810f6d250db6e8e756872ea1f6ca488c9ba2
97110324 <97110324@st.deltion.nl>
2025-04-02 15:34:52 +0200

    klaar

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -4,4 +4,34 @@
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+
+gasten = []
+
+# Voeg de gasten toe aan de lijst
+gasten.append("Jij")
+gasten.append("Paul")
+gasten.append("Kees")
+gasten.append("Marie")
+gasten.append("Hilda")
+
+# Print de lijst met alle gasten
+print("Lijst met gasten na het toevoegen:")
+print(gasten)
+
+# Marie belt dat ze niet meer meegaat, haal haar uit de lijst
+gasten.remove("Marie")
+
+# Print de lijst zonder Marie
+print("\nLijst met gasten nadat Marie afzegt:")
+print(gasten)
+
+# George wil ook mee en wil naast Kees zitten
+# Vind de index van Kees in de lijst
+kees_index = gasten.index("Kees")
+
+# Voeg George toe naast Kees (index + 1)
+gasten.insert(kees_index + 1, "George")
+
+# Print de uiteindelijke lijst met gasten
+print("\nUiteindelijke lijst met gasten:")
+print(gasten)
