c2cd45b46a00bde1e4d4c76cf0f75eb583cc7e1a
97110324 <97110324@st.deltion.nl>
2025-04-02 15:11:23 +0200

    klaar

--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -3,14 +3,19 @@
 # Groep:
 
 # Hier komt je code...
+# Opdracht 3 tekstfuncties# Groep:
+
+# Test 1: x = 1
 x = 1
-y = ...
-print( ... )
+y = 4 * (x ** 3) - 2 * (x ** 2) + 1
+print(f"Voor x = {x}, y = {y}")  # Output: Voor x = 1, y = 3
 
+# Test 2: x = 2
 x = 2
-y = ...
-print( ... )
+y = 4 * (x ** 3) - 2 * (x ** 2) + 1
+print(f"Voor x = {x}, y = {y}")  # Output: Voor x = 2, y = 25
 
+# Test 3: x = 0
 x = 0
-y = ...
-print( ... )
+y = 4 * (x ** 3) - 2 * (x ** 2) + 1
+print(f"Voor x = {x}, y = {y}")  # Output: Voor x = 0, y = 1
