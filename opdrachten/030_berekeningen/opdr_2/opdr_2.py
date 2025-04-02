f0619dfb6e4abf22c6271e8496f43784aa2cc4dd
97110324 <97110324@st.deltion.nl>
2025-04-02 14:55:20 +0200

    afgemaakt

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,18 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
 
-print()
+c1 = -12
+f1 = 102
+
+
+c2 = 62.2
+f2 = 96
+
+# Berekeningen en output voor de eerste set waarden
+print(f"{c1} graden Celsius is gelijk aan {round(c1 * (9 / 5) + 32, 1)} graden Fahrenheit")
+print(f"{f1} graden Fahrenheit is gelijk aan {round((f1 - 32) * (5 / 9), 1)} graden Celsius\n")
+
+# Berekeningen en output voor de tweede set waarden
+print(f"{c2} graden Celsius is gelijk aan {round(c2 * (9 / 5) + 32, 1)} graden Fahrenheit")
+print(f"{f2} graden Fahrenheit is gelijk aan {round((f2 - 32) * (5 / 9), 1)} graden Celsius")
