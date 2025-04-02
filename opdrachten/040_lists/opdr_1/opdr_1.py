e31d5474873cc6ccc3e7f74c7a93b39970005b0d
97110324 <97110324@st.deltion.nl>
2025-04-02 15:24:39 +0200

    klaar

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,21 @@
 # Opdracht 1 lists
-# Naam student:
-# Groep:
+# Naam student: [ Jouw naam ]
+# Groep: [ Jouw groep ]
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+# Maak een lege list
+mylist = []
+
+# Maak 4 dictionaries voor personen
+dict_1 = { "id": 0, "voornaam": "Jan", "achternaam": "Jansen" }
+dict_2 = { "id": 1, "voornaam": "Piet", "achternaam": "Pietersen" }
+dict_3 = { "id": 2, "voornaam": "Klaas", "achternaam": "Klaassen" }
+dict_4 = { "id": 3, "voornaam": "Cor", "achternaam": "Corsi" }
+
+# Voeg de dictionaries toe aan de list met de append-methode
+mylist.append(dict_1)
+mylist.append(dict_2)
+mylist.append(dict_3)
+mylist.append(dict_4)
+
+# Print de volledige naam van de 2e persoon
+print(mylist[1]["voornaam"], mylist[1]["achternaam"])
