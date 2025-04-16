5a7ae999758ac31d33ff69070feb0cbd7a27ea07
97110324 <97110324@st.deltion.nl>
2025-04-16 15:11:01 +0200

    Klaar

--- opdrachten/070_condities/opdr_2/opdr_2.py
+++ opdrachten/070_condities/opdr_2/opdr_2.py
@@ -8,6 +8,17 @@
 
 my_list = [43948, 878768, 38768, 87555, 765765]
 
+# Maak een lege lijst om de resultaten op te slaan
+deelbaar_door_3 = []
+
+# Loop door elk getal in de lijst
+for getal in my_list:
+    if getal % 3 == 0:  # Controleer of het getal deelbaar is door 3
+        deelbaar_door_3.append(getal)  # Voeg het getal toe aan de nieuwe lijst
+
+# Print de resulterende lijst
+print(deelbaar_door_3)
+
 # for...:
 #   if...:
 #       print()
