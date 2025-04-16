e65622237f7c41d73d90131f725153f1b2317baf
97110324 <97110324@st.deltion.nl>
2025-04-16 15:15:06 +0200

    Klaar

--- opdrachten/070_condities/opdr_3/opdr_3.py
+++ opdrachten/070_condities/opdr_3/opdr_3.py
@@ -4,9 +4,33 @@
 
 
 
-
 normale_toegangsprijs = 12.50
 kortings_percentages = {"baby": 100, "kinderen": 50, "volwassenen": 0, "ouderen": 30}
-leeftijd = {"baby": (0, 2), "kinderen": (3, 18), "volwassenen": (19, 64), "ouderen": (65, 150)}
+leeftijdsgroepen = {
+    "baby": (0, 2),
+    "kinderen": (3, 18),
+    "volwassenen": (19, 64),
+    "ouderen": (65, 150),
+}
+
+leeftijd_input = int(input("Geef uw leeftijd in aantal jaar: "))
+
+groep = None
+for categorie, (min_leeftijd, max_leeftijd) in leeftijdsgroepen.items():
+    if min_leeftijd <= leeftijd_input <= max_leeftijd:
+        groep = categorie
+        break
+
+if groep is None:
+    print("Ongeldige leeftijd!")
+else:
+
+    korting_percentage = kortings_percentages[groep]
+    korting_bedrag = normale_toegangsprijs * (korting_percentage / 100)
+    te_betalen_prijs = normale_toegangsprijs - korting_bedrag
+
+    print(f"U behoort tot de groep {groep}")
+    print(f"U krijgt {korting_percentage}% korting")
+    print(f"U betaalt daarom {te_betalen_prijs:.2f}")
 
 # Hier komt je code...
