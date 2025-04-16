fd0fb743a288d71ffc0f6bd3cbb91cde60e61a38
97110324 <97110324@st.deltion.nl>
2025-04-16 14:46:25 +0200

    Klaar

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,17 @@
 
 # Hier komt je code...
 
+invoer = input("Voer minimaal 5 objecten in, gescheiden door komma's: ")
+
+
+objecten = [item.strip() for item in invoer.split(",")]
+
+
+if len(objecten) < 5:
+    print("Je moet minimaal 5 objecten invoeren!")
+else:
+
+    objecten.sort(reverse=True)
+
+
+print(objecten)
