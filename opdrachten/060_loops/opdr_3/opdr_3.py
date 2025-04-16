de5a4aadbaa18cb947d1f4901f9b1a3888988682
97110324 <97110324@st.deltion.nl>
2025-04-16 14:56:12 +0200

    Klaar

--- opdrachten/060_loops/opdr_3/opdr_3.py
+++ opdrachten/060_loops/opdr_3/opdr_3.py
@@ -7,3 +7,11 @@
 # Hier start de for-loop
 
 my_list = []
+
+
+for i in range(3, 82, 3):  # Begin bij 3, eindig bij (en inclusief) 81, tel telkens 3 op
+    berekening = (i ** 2) / 3  # Bereken het kwadraat van i en deel het door 3
+    my_list.append(berekening)  # Voeg het resultaat toe aan de lijst
+
+# Print de resulterende lijst
+print(my_list)
