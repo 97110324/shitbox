6b63329aad6c2fb1e9c68cedd728239b8fea8f77
97110324 <97110324@st.deltion.nl>
2025-04-16 15:03:10 +0200

    Klaar

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -6,4 +6,24 @@
 
 # Hier start de for-loop
 
-my_list = []
+
+
+pizzas = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+
+pizzas.sort()
+print("Gesorteerde lijst:", pizzas)
+
+pizzas.append('yo-favorito')
+print("Lijst met nieuwe pizza:", pizzas)
+
+pizzas.remove('olivio')
+print("Lijst na verwijderen van minst favoriete pizza:", pizzas)
+
+eerste_drie = pizzas[:3]
+print("Eerste 3 pizza's:", eerste_drie)
+
+middelste_pizza = pizzas[len(pizzas) // 2]
+print("Middelste pizza:", [middelste_pizza])
+
+laatste_drie = pizzas[-3:]
+print("Laatste 3 pizza's:", laatste_drie)
