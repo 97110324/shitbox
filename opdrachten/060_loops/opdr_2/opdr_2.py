0f86ddfd5e3704fc07b13d60d3af2870e7dd45f0
97110324 <97110324@st.deltion.nl>
2025-04-16 14:54:34 +0200

    Klaar

--- opdrachten/060_loops/opdr_2/opdr_2.py
+++ opdrachten/060_loops/opdr_2/opdr_2.py
@@ -8,3 +8,9 @@
 
 my_list = []
 
+# Gebruik een for-loop en range() om de lijst te vullen
+for i in range(1, 6):  # range(1, 6) genereert getallen van 1 tot en met 5
+    my_list.append(i * 10)  # Voeg het 10-voudige van i toe aan de lijst
+
+# Print de lijst op het scherm
+print(my_list)
