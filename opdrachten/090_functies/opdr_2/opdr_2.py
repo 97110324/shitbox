0386e27c1251881d4d93e0fab9e4b1bf36a25bd7
97110324 <97110324@st.deltion.nl>
2025-04-16 16:01:51 +0200

    Klaar

--- opdrachten/090_functies/opdr_2/opdr_2.py
+++ opdrachten/090_functies/opdr_2/opdr_2.py
@@ -2,19 +2,22 @@
 # Naam student:
 # Groep:
 
+CONVERSIEFACTOR = 1.609344
 
 def kilometers_naar_miles(km):
-    # je code komt hier
+    return km / CONVERSIEFACTOR
     # het woordje pass hieronder kun je weghalen
-    pass
+
 
 def miles_naar_kilometers(miles):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    return miles * CONVERSIEFACTOR    # het woordje pass hieronder kun je weghalen
+
 
 kilometers = 1223
 miles = 867
 
-print(kilometers_naar_miles(kilometers))
-print(miles_naar_kilometers(miles))
+miles_resultaat = kilometers_naar_miles(kilometers)
+print(f"{kilometers} kilometers = {miles_resultaat} miles")
+
+km_resultaat = miles_naar_kilometers(miles)
+print(f"{miles} miles = {km_resultaat} kilometers")
